22b226665d761d3351438234082db3e7fea3597a
Ryan Han <ryan.han@uwaterloo.ca>
2023-12-29 13:48:53 -0500

    fix: fix packages and link component

--- src/content/config.ts
+++ src/content/config.ts
@@ -21,5 +21,5 @@ const blogCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-	blog: blogCollection,
+	ourwork: ourworkCollection,
 };
